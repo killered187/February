e28fc5be63acd204adce8793e37ef51c51e36eb4
game110pm <game110pm@605-110-STU13>
2017-02-09 13:13:49 -0700

    Warm Up

---
+++ 2.08.17/2.08.17/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int width;
+	int height;
+	int volume;
+	cout << "Give me the height and width.";
+	cin >> width;
+	cin >> height;
+	volume = height * width / 3;
+}
